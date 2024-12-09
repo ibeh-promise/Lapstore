a26992ba3684cc453e956cc1d753e469a806d27f
cd-pixel <daviddominic767@gmail.com>
2024-12-09 04:16:12 -0800

    added some extra features

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const Router = require("./router/router");
+const path = require("path")
 const app = express();
 const port = 3000;
 
@@ -11,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Use router
+app.use(express.static(path.join(__dirname, "public")));
 app.use(Router);
 
 app.listen(port, () => {
